fbf0628cb61242fe8e6fc124a9749e0be61163ae
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2021-01-27 15:42:34 -0300

    Project created

---
+++ src/main/java/com/thiago/webservicespringbootjpahibernate/WebserviceSpringbootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.webservicespringbootjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceSpringbootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceSpringbootJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/webservicespringbootjpahibernate/WebserviceSpringbootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.webservicespringbootjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceSpringbootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
