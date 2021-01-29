d9aebb95fb56ca2d9b1473843d479493510e8669
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2021-01-29 17:27:24 -0300

    User entity and Resource

---
+++ src/main/java/com/thiago/webservicespringbootjpahibernate/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.thiago.webservicespringbootjpahibernate.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thiago.webservicespringbootjpahibernate.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"maria","maria@","9999","1234");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
