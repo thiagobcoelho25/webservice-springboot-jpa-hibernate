6708a72107f766c60a74f157711d3c8d53a161e6
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2021-02-06 16:05:43 -0300

    Exception handling - delete

--- src/main/java/com/thiago/webservicespringbootjpahibernate/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/thiago/webservicespringbootjpahibernate/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.thiago.webservicespringbootjpahibernate.services.exceptions.DatabaseException;
 import com.thiago.webservicespringbootjpahibernate.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -20,6 +21,13 @@ public class ResourceExceptionHandler {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
-		
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
 	}
 }
